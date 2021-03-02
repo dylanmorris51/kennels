f494253a44e99e16753d78f6adb16d46b53708c8
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-02 11:19:23 -0600

    refactored Kennel module for authentication. Must download install packs

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -10,12 +10,34 @@ import { CustomerCard } from "./customer/Customer"
 import { PropsAndState } from "./PropsAndState"
 import { Home } from "./Home"
 import { Link } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const Kennel = () => (
     <>
-        <NavBar />
-        <ApplicationViews />                
+        <Route 
+            render={() => {
+                if (localStorage.getItem("kennel_customer")) {
+                    return (
+                        <>
+                            <NavBar />
+                            <ApplicationViews />                
+                        </>
+                    )
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
+
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
     </>
 )
-    
+
 
